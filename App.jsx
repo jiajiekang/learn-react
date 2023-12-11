0f1ffbe07e96091426bf94d58782c61645dbae69
jiajiekang <jjkang@live.com>
2023-12-11 11:03:27 +0800

    Adjust H1 class and body margin

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Dtone() {
   return (
     <>
-      <h1 className="dtone">Search Input</h1>
+      <h1 className="title">Search Input</h1>
     </>
   );
 }
