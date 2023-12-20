8c486222c94f775e8b204cd41891926d38574d0f
Jiajie Kang <jiajie.kang@dtone.com>
2023-12-21 05:46:18 +0800

    Curly Brace: Todolist

--- App.jsx
+++ App.jsx
@@ -1,14 +1,28 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
-}
+const baseUrl = "https://i.imgur.com/";
+const person = {
+  name: "Gregorio Y. Zara",
+  imageId: "7vQD0fP",
+  imageSize: "s",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
 
-export default function Gallery() {
+export default function TodoList() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
---
+++ TodoList.jsx
@@ -0,0 +1,9 @@
+const today = new Date();
+
+function formatData(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
+
+export default function TodoList() {
+  return <h1>To Do List for {formatData(today)}</h1>;
+}
