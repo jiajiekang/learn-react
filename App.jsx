c25df8ec321330dcb52c2c7ed167081a7b77c2d7
Jiajie Kang <jiajie.kang@dtone.com>
2023-12-23 05:09:56 +0800

    Passing Props: Chanllenge 1, Extract a component

--- App.jsx
+++ App.jsx
@@ -1,19 +1,55 @@
-import Avatar from "./Avatar";
+import { getImageUrl } from "./utils.js";
 
-function Card({ children }) {
-  return <div className="card">{children}</div>;
+function Profile({ imageId, name, profession, awards, discovery, imageSize = 70 }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+      />
+      <ul>
+        <li>
+          <b>Profession:</b> {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>({awards.join(", ")})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-export default function Profile() {
+export default function Gallery() {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: "Katsuko Saruhashi",
-          imageId: "YfeOqp2",
-        }}
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        imageId="szV5sdG"
+        name="Maria Skłodowska-Curie"
+        profession="physicist and chemist"
+        discovery="polonium (chemical element)"
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal",
+          "Matteucci Medal",
+        ]}
       />
-    </Card>
+      <Profile
+        imageId="YfeOqp2"
+        name="Katsuko Saruhashi"
+        profession="geochemist"
+        discovery="a method for measuring carbon dioxide in seawater"
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+      />
+    </div>
   );
 }
