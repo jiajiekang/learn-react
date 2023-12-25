381e3f3aa3b625959511862ec5f5a8ff323ad902
Jiajie Kang <jiajie.kang@dtone.com>
2023-12-25 11:58:32 +0800

    Rendering list: Chanllenge 3

--- App.jsx
+++ App.jsx
@@ -1,18 +1,24 @@
 import { recipes } from "./data";
 
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe {...recipe} key={recipe.id} />
       ))}
     </div>
   );
