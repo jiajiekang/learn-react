3015b314b79332a17a14585e4f598ff3fa7747c7
Jiajie Kang <jiajie.kang@dtone.com>
2023-12-29 11:53:33 +0800

    State: challeng 2

--- App.jsx
+++ App.jsx
@@ -1,48 +1,34 @@
 import { useState } from "react";
-import { sculptureList } from "./data";
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  function handlePrevClick() {
-    if (hasPrev) {
-      setIndex(index - 1);
-    }
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    }
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handlePrevClick} disabled={!hasPrev}>
-        Prev
-      </button>
-      <button onClick={handleNextClick} disabled={!hasNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <button onClick={handleMoreClick}>{showMore ? "Hide" : "Show"} details</button>
-      {showMore && <p>{sculpture.description}</p>}
-    </>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input placeholder="Last name" value={lastName} onChange={handleLastNameChange} />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
