58c37177cf4200c8ebdfeb6df1e46b8261ea1479
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-01 09:51:48 +0800

    Updating Objects in State: Updating a nested object

--- App.jsx
+++ App.jsx
@@ -2,35 +2,77 @@ import { useState } from "react";
 
 export default function Form() {
   const [person, setPerson] = useState({
-    firstName: "Barbara",
-    lastName: "Hepworth",
-    email: "bhepworth@sculpture.com",
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
   });
 
-  function handleChange(e) {
+  function handleNameChange(e) {
     setPerson({
       ...person,
-      [e.target.name]: e.target.value,
+      name: e.target.value,
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value,
+      },
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
     });
   }
 
   return (
     <>
       <label>
-        First name:
-        <input name="firstName" value={person.firstName} onChange={handleChange} />
+        Name:
+        <input value={person.name} onChange={handleNameChange} />
       </label>
       <label>
-        Last name:
-        <input name="lastName" value={person.lastName} onChange={handleChange} />
+        Title:
+        <input value={person.artwork.title} onChange={handleTitleChange} />
       </label>
       <label>
-        Email:
-        <input name="email" value={person.email} onChange={handleChange} />
+        City:
+        <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <label>
+        Image:
+        <input value={person.artwork.image} onChange={handleImageChange} />
       </label>
       <p>
-        {person.firstName} {person.lastName} ({person.email})
+        <i>{person.artwork.title}</i>
+        {" by "}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
       </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
     </>
   );
 }
