d19bbaeab07c6da0ee989b027be5771154f80b76
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-02 16:57:43 +0800

    Managing State: challenge 1

--- App.jsx
+++ App.jsx
@@ -1,58 +1,27 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("typing");
+export default function Picture() {
+  const [isActive, setIsActive] = useState(false);
 
-  if (status === "success") {
-    return <h1>That{"'"}s right!</h1>;
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
+  let backgroundClassName = "background";
+  let pictureClassName = "picture";
+  if (isActive) {
+    pictureClassName += " picture--active";
+  } else {
+    backgroundClassName += " background--active";
   }
 
   return (
-    <>
-      <h2>City quiz</h2>
-      <p>In which city is there a billboard that turns air into drinkable water?</p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === "submitting"}
-        />
-        <br />
-        <button disabled={answer.length === 0 || status === "submitting"}>Submit</button>
-        {error !== null && <p className='Error'>{error.message}</p>}
-      </form>
-    </>
+    <div className={backgroundClassName} onClick={() => setIsActive(false)}>
+      <img
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsActive(true);
+        }}
+        className={pictureClassName}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+      />
+    </div>
   );
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== "lima";
-      if (shouldError) {
-        reject(new Error("Good guess but a wrong answer. Try again!"));
-      } else {
-        resolve();
-      }
-    }, 1500);
-  });
 }
