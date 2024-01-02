f4187f344c07dd0c981b592691374129739f4ef6
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-02 14:49:58 +0800

    Managing State: Reacting to input with state

--- App.jsx
+++ App.jsx
@@ -1,52 +1,58 @@
 import { useState } from "react";
-import AddTodo from "./AddTodo";
-import TaskList from "./TaskList";
 
-let nextId = 3;
-const initialTodos = [
-  { id: 0, title: "Buy milk", done: true },
-  { id: 1, title: "Eat tacos", done: false },
-  { id: 2, title: "Brew tea", done: false },
-];
+export default function Form() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
 
-export default function TaskApp() {
-  const [todos, setTodos] = useState(initialTodos);
-
-  function handleAddTodo(title) {
-    setTodos([
-      ...todos,
-      {
-        id: nextId++,
-        title: title,
-        done: false,
-      },
-    ]);
+  if (status === "success") {
+    return <h1>That{"'"}s right!</h1>;
   }
 
-  function handleChangeTodo(nextTodo) {
-    setTodos(
-      todos.map((t) => {
-        if (t.id === nextTodo.id) {
-          return nextTodo;
-        } else {
-          return t;
-        }
-      }),
-    );
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err);
+    }
   }
 
-  function handleDeleteTodo(todoId) {
-    setTodos(todos.filter((t) => t.id !== todoId));
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
   }
 
   return (
     <>
-      <AddTodo onAddTodo={handleAddTodo} />
-      <TaskList
-        todos={todos}
-        onChangeTodo={handleChangeTodo}
-        onDeleteTodo={handleDeleteTodo}
-      />
+      <h2>City quiz</h2>
+      <p>In which city is there a billboard that turns air into drinkable water?</p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === "submitting"}
+        />
+        <br />
+        <button disabled={answer.length === 0 || status === "submitting"}>Submit</button>
+        {error !== null && <p className='Error'>{error.message}</p>}
+      </form>
     </>
   );
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== "lima";
+      if (shouldError) {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
