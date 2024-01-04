2e510fc44f36127c6b3d71178d0a77935ed2b5f0
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-04 09:34:04 +0800

    Choosing state structure: Avoid deeply nested state

--- App.jsx
+++ App.jsx
@@ -1,55 +1,71 @@
 import { useState } from "react";
+import { initialTravelPlan } from "./places";
 
-const initialItems = [
-  { title: "pretzels", id: 0 },
-  { title: "crispy seaweed", id: 1 },
-  { title: "granola bar", id: 2 },
-];
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
 
-export default function Menu() {
-  const [items, setItems] = useState(initialItems);
-  const [selectedId, setSelectedId] = useState(0);
-
-  const selectedItem = items.find((item) => item.id === selectedId);
-
-  function handleItemChange(id, e) {
-    setItems(
-      items.map((item) => {
-        if (item.id === id) {
-          return {
-            ...item,
-            title: e.target.value,
-          };
-        } else {
-          return item;
-        }
-      }),
-    );
+  function handleComplete(parentId, childId) {
+    const parent = plan[parentId];
+    // Create a new version of the parent place
+    // that doesn't include this child ID.
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter((id) => id !== childId),
+    };
+    // Update the root state object...
+    setPlan({
+      ...plan,
+      // ...so that it has the updated parent.
+      [parentId]: nextParent,
+    });
   }
 
+  const root = plan[0];
+  const planetIds = root.childIds;
   return (
     <>
-      <h2>Whats your travel snack?</h2>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <input
-              value={item.title}
-              onChange={(e) => {
-                handleItemChange(item.id, e);
-              }}
-            />{" "}
-            <button
-              onClick={() => {
-                setSelectedId(item.id);
-              }}
-            >
-              Choose
-            </button>
-          </li>
+      <h2>Places to visit</h2>
+      <ol>
+        {planetIds.map((id) => (
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
         ))}
-      </ul>
-      <p>You picked {selectedItem.title}.</p>
+      </ol>
     </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button
+        onClick={() => {
+          onComplete(parentId, id);
+        }}
+      >
+        Complete
+      </button>
+      {childIds.length > 0 && (
+        <ol>
+          {childIds.map((childId) => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      )}
+    </li>
   );
 }
