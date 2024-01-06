eedc1654672f33586509fa212237c0f82b181e8b
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-07 07:05:39 +0800

    Context: an alternative to passing props

--- App.jsx
+++ App.jsx
@@ -1,26 +1,25 @@
-import { useReducer } from "react";
-import Chat from "./Chat";
-import ContactList from "./ContactList";
-import { initialState, messengerReducer } from "./messengerReducer";
+import Heading from "./Heading";
+import Section from "./Section";
 
-export default function Messenger() {
-  const [state, dispatch] = useReducer(messengerReducer, initialState);
-  const message = state.messages[state.selectedId];
-  const contact = contacts.find((c) => c.id === state.selectedId);
+export default function Page() {
   return (
-    <div>
-      <ContactList
-        contacts={contacts}
-        selectedId={state.selectedId}
-        dispatch={dispatch}
-      />
-      <Chat key={contact.id} message={message} contact={contact} dispatch={dispatch} />
-    </div>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
-
-const contacts = [
-  { id: 0, name: "Taylor", email: "taylor@mail.com" },
-  { id: 1, name: "Alice", email: "alice@mail.com" },
-  { id: 2, name: "Bob", email: "bob@mail.com" },
-];
