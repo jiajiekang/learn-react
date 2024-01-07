272484a776f47b1e861f1125ba5b98885036d227
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-07 09:48:16 +0800

    Context: challenge

--- App.jsx
+++ App.jsx
@@ -1,42 +1,53 @@
-import Heading from "./Heading";
-import Section from "./Section";
+import { useState, useContext } from "react";
+import { places } from "./data.js";
+import { getImageUrl } from "./utils.js";
+import { ImageSizeContext } from "./Context.js";
 
-export default function ProfilePage() {
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
   return (
-    <Section>
-      <Heading>My Profile</Heading>
-      <Post title="Hello traveller!" body="Read about my adventures." />
-      <AllPosts />
-    </Section>
+    <ImageSizeContext.Provider value={imageSize}>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={(e) => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Use large images
+      </label>
+      <hr />
+      <List />
+    </ImageSizeContext.Provider>
   );
 }
 
-function AllPosts() {
-  return (
-    <Section>
-      <Heading>Posts</Heading>
-      <RecentPosts />
-    </Section>
-  );
+function List() {
+  const listItems = places.map((place) => (
+    <li key={place.id}>
+      <Place place={place} />
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
 }
 
-function RecentPosts() {
+function Place({ place }) {
   return (
-    <Section>
-      <Heading>Recent Posts</Heading>
-      <Post title="Flavors of Lisbon" body="...those pastéis de nata!" />
-      <Post title="Buenos Aires in the rhythm of tango" body="I loved it!" />
-    </Section>
-  );
-}
-
-function Post({ title, body }) {
-  return (
-    <Section isFancy={true}>
-      <Heading>{title}</Heading>
+    <>
+      <PlaceImage place={place} />
       <p>
-        <i>{body}</i>
+        <b>{place.name}</b>
+        {": " + place.description}
       </p>
-    </Section>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const imageSize = useContext(ImageSizeContext);
+  return (
+    <img src={getImageUrl(place)} alt={place.name} width={imageSize} height={imageSize} />
   );
 }
