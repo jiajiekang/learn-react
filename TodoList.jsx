1ea820b55fd861806689aace1a99019a0826e83e
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-08 11:07:11 +0800

    Remove files

--- TodoList.jsx
+++
@@ -1,9 +0,0 @@
-const today = new Date();
-
-function formatData(date) {
-  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-}
-
-export default function TodoList() {
-  return <h1>To Do List for {formatData(today)}</h1>;
-}
