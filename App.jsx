0101b1263f0824f1fac89548d60743191b54b95c
Jiajie Kang <jiajie.kang@dtone.com>
2024-01-09 12:38:55 +0800

    Effect: challenge 1

--- App.jsx
+++ App.jsx
@@ -1,41 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import MyInput from "./MyInput.js";
 
-function Playground() {
-  const [text, setText] = useState("a");
-
-  useEffect(() => {
-    function onTimeout() {
-      console.log("⏰ " + text);
-    }
-
-    console.log('🔵 Schedule "' + text + '" log');
-    const timeoutId = setTimeout(onTimeout, 3000);
-
-    return () => {
-      console.log('🟡 Cancel "' + text + '" log');
-      clearTimeout(timeoutId);
-    };
-  }, [text]);
-
-  return (
-    <>
-      <label>
-        What to log: <input value={text} onChange={(e) => setText(e.target.value)} />
-      </label>
-      <h1>{text}</h1>
-    </>
-  );
-}
-
-export default function App() {
+export default function Form() {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState("Taylor");
+  const [upper, setUpper] = useState(false);
   return (
     <>
-      <button onClick={() => setShow(!show)}>
-        {show ? "Unmount" : "Mount"} the component
-      </button>
-      {show && <hr />}
-      {show && <Playground />}
+      <button onClick={() => setShow((s) => !s)}>{show ? "Hide" : "Show"} form</button>
+      <br />
+      <hr />
+      {show && (
+        <>
+          <label>
+            Enter your name:
+            <MyInput value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={upper}
+              onChange={(e) => setUpper(e.target.checked)}
+            />
+            Make it uppercase
+          </label>
+          <p>
+            Hello, <b>{upper ? name.toUpperCase() : name}</b>
+          </p>
+        </>
+      )}
     </>
   );
 }
